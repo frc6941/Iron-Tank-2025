99432369d09201a8ba5f886ee0921c62eeaa4240
AlltAWD <alltawd@gmail.com>
2025-07-19 23:59:33 +0800

    deploy event_Sanya

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Iron-Tank-2025";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 79;
+  public static final String GIT_SHA = "7a59e0cef17589cf7f006fd43059afec4362768b";
+  public static final String GIT_DATE = "2025-07-19 23:53:01 CST";
+  public static final String GIT_BRANCH = "event_Sanya";
+  public static final String BUILD_DATE = "2025-07-19 23:59:12 CST";
+  public static final long BUILD_UNIX_TIME = 1752940752970L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
