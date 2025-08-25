fbe8287aea8c86872f19768402c77501334de00c
rubyLu46 <ruby_luweitong@163.com>
2025-08-25 13:30:25 +0800

    done

--- src/main/java/frc/robot/subsystems/Intake/PivotIOReal.java
+++ src/main/java/frc/robot/subsystems/Intake/PivotIOReal.java
@@ -43,6 +43,7 @@ public class PivotIOReal implements PivotIO {
                 velocityRotPerSec,
                 appliedVolts,
                 motorVolts,
+                
                 statorCurrentAmps,
                 supplyCurrentAmps,
                 tempCelsius,
